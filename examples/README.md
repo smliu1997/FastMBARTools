38227e893636aaf6fe8df7057f8a5d6eb349a6a1
Shuming Liu <smliu@mit.edu>
2023-04-23 23:18:26 -0400

    Modify, add temperature replica exchange example

---
+++ examples/README.md
@@ -0,0 +1,8 @@
+# examples
+
+Examples of using fastmbartools. 
+
+butane-umbrella is an example of computing PMF along dihedral with umbrella sampling data.
+
+hp1alpha-TRE is an example of computing PMF along Rg for HP1alpha dimer protein with temperature replica exchange simulation data. 
+
--- fastmbartools/solver.py
+++ fastmbartools/solver.py
@@ -13,33 +13,40 @@ bias_dict = {'linear': bias.linear_bias,
 class FastMBARSolver(object):
     """
     The class for efficiently solving MBAR equations rapidly with FastMBAR. 
+    Some variable names follow the conventions in FastMBAR tutorial. 
+    This is espeically convenient for linear bias, harmonic bias, and temperature replica simulations. 
     """
-    def __init__(self, T, cv, bias_param, bias_func='harmonic', kB=8.314e-3, unbiased_energy=None):
+    def __init__(self, T, cv, bias_param=None, bias_func=None, kB=8.314e-3, unbiased_energy=None):
         """
         Initialize. 
         
         Parameters
         ----------
         T : float or int or 1d array like
-            Temperatures for the trajectories. 
-            If temperatures is float or int, all the simulations share the temperature. 
+            Temperatures for the states. 
+            If temperatures is float or int, all the simulations share the same temperature. 
             If temperatures is array like, it gives the temperature for each simulation. 
         
         cv : array like, cv[i] is 1d or 2d array like
-            Collective variables of trajectories. 
+            Collective variables of the states. 
             cv[i] includes samples from the i-th state (state means given temperature and bias).
             len(cv) should be the number of states. 
-            1d cv[i] is only available if target collective variable is 1d.
+            1d cv[i] is only available if target collective variable is 1d, and 1d cv[i] is converted to 2d array internally.
             For general cases, cv[i] is 2d. 
+            Note len(cv[i]) does not have to be equal for different i. 
         
-        bias_param : array like, bias_param[i] should be 1d array like
-            Parameters for the bias. len(bias_param) should be the number of states. 
+        bias_param : None or array like, the format of bias_param[i] depends on the bias function.
+            Parameters for the bias. 
+            If None, no bias parameter is provided. 
+            If bias_param is array like, len(bias_param) should be the number of states. 
+            If bias_param is array like and p = bias_param[i] is None, then the i-th bias function does not have input parameter. 
         
-        bias_func : str or function or list of functions
+        bias_func : None or str or function or list of functions
             Bias function. 
-            If bias_type is string, it has to be 'linear' or 'harmonic', which means all the potentials are linear or harmonic.
-            If bias_type is a function, this function is applied to every state. 
-            If bias_type is a list, it provides potentials for all the states. Note FastMBAR is versatile and each state can have a different functional form. 
+            If bias_func is None, there is no additional bias. 
+            If bias_func is string, it has to be 'linear' or 'harmonic', which means all the potentials are linear or harmonic.
+            If bias_func is a function, this function is applied to every state. 
+            If bias_func is a list, it provides potentials for all the states. Note FastMBAR is versatile and each state can have a different functional form. 
         
         kB : float or int
             Boltzmann constant for the given temperature unit and energy unit.
@@ -64,12 +71,16 @@ class FastMBARSolver(object):
                 X.append(j)
         X = np.array(X)
         if X.ndim == 1:
+            # convert to 2d
             X = np.reshape(X, (-1, 1))
         self.X = X # put all the samples together
         assert self.X.ndim == 2
-        assert len(bias_param) == self.M
+        if bias_param is not None:
+            assert len(bias_param) == self.M
         self.bias_param = bias_param
-        if isinstance(bias_func, str):
+        if bias_func is None:
+            self.bias_func = [None]*self.M
+        elif isinstance(bias_func, str):
             self.bias_func = []
             for i in range(self.M):
                 self.bias_func.append(bias_dict[bias_func])
@@ -107,7 +118,14 @@ class FastMBARSolver(object):
         """
         A = np.zeros((self.M, self.X.shape[0])) + self.E0
         for i in range(self.M):
-            A[i, :] += self.bias_func[i](self.X, self.bias_param[i])
+            f = self.bias_func[i]
+            if f is not None:
+                p = self.bias_param[i]
+                if p is None:
+                    # the bias potential only needs self.X as input
+                    A[i, :] += f(self.X)
+                else:
+                    A[i, :] += f(self.X, p)
             A[i, :] /= (self.kB*self.T[i])
         self.A = A
         self.F = FastMBAR(energy=self.A, num_conf=self.n_samples, cuda=cuda, bootstrap=bootstrap, verbose=verbose)
@@ -115,6 +133,7 @@ class FastMBARSolver(object):
     def compute_pmf(self, target_T, target_cv, bins, min_PMF_as_zero=True):
         """
         Compute PMF along the target collective variables. 
+        PMF is computed by dividing target_cv into grids, and each grid represents a state. 
         This only supports computing 1d or 2d or 3d target collective variables. 
         
         Parameters
@@ -149,10 +168,11 @@ class FastMBARSolver(object):
         if Y.ndim == 1:
             Y = np.reshape(Y, (-1, 1))
         d = Y.shape[1]
+        assert d <= 3
         assert len(bins) == d
         L = 1
         for each in bins:
-            L *= (len(each) - 1)
+            L *= (len(each) - 1) # L is the total number of grids
         centers = []
         for each in bins:
             centers.append(0.5*(each[1:] + each[:-1]))
@@ -169,12 +189,11 @@ class FastMBARSolver(object):
                 for j in centers[1]:
                     for k in centers[2]:
                         ext_centers.append([i, j, k])
-        else:
-            sys.exit('target_cv dimension is larger than 3.')
         ext_centers = np.array(ext_centers)
         assert ext_centers.ndim == 2
         R = np.zeros((L, Y.shape[0]))
         for i in range(Y.shape[0]):
+            # assign the sample into the corresponding grid
             H_i, _ = np.histogramdd(Y[[i]], bins)
             H_i[H_i == 0] = np.inf
             H_i[H_i == 1] = 0
